df59cee3c61269e78aa7872409a13a8119818b69
Borodin Dan <56741340+danborodin@users.noreply.github.com>
2020-11-01 23:02:18 +0200

    Update main.go

--- main.go
+++ main.go
@@ -5,13 +5,9 @@ import (
 	"net/http"
 
 	"github.com/danborodin/go-chat-server/router"
-	"github.com/gorilla/websocket"
 )
 
 func main() {
-	// setupRoutes()
-	// log.Fatal(http.ListenAndServe(":8080", nil))
-
 	router.SetupRoutes()
 	log.Printf("Server running at address %s", router.Host)
 	log.Fatal(http.ListenAndServe(router.Host, router.Router))
